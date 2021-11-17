35e5f74d8887dcc1dfff64224dd627db6053f026
Haris <sultanic.haris@gmail.com>
2021-11-17 13:19:26 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        create: './src/createElement.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restourant Page',
+        }),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|webp)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        runtimeChunk: 'single'
+    },
+};
